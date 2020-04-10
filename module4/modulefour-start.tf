b96f4191f9ed8a49dcfaca8b53208020564b8369
Diego Mayorga <diego.mayorga86@gmail.com>
2020-04-10 20:07:21 -0300

    add missing semi colon

--- module4/modulefour-start.tf
+++ module4/modulefour-start.tf
@@ -132,8 +132,8 @@ resource "aws_instance" "nginx1" {
 
 	provisioner "remote-exec" {
 		inline = [
-			"sudo yum install nginx -y"
-			"sudo service nginx start"
+			"sudo yum install nginx -y",
+			"sudo service nginx start",
 			"echo '<html><head><title>Super Mario Team</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">Blue Team</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html"
 		]
 	}
