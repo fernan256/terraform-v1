3a38b7792554d4a3156eb7593be955ecea127219
Diego Mayorga <diego.mayorga86@gmail.com>
2020-04-15 20:18:10 -0300

    Change to use env vars to aws creds

--- module7/outputs.tf
+++ module7/outputs.tf
@@ -1,10 +1,10 @@
 # OUTPUTS
 ###########################################################################
  
- output "aws_elb_public_dns" {
-     value = aws_elb_web.dns_name
- }
+output "aws_elb_public_dns" {
+	value = aws_elb.web.dns_name
+}
 
  output "cname_record_url" {
-     value = "http://${local.env_name}-website.${var.dns_zone_name}"
+  value = "http://${local.env_name}-website.${var.dns_zone_name}"
  }
--- module7/variables.tf
+++ module7/variables.tf
@@ -1,8 +1,8 @@
 # VARIABLES
 ##################################################################################
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
 variable "private_key_path" {}
 variable "key_name" {}
 variable "region" {
