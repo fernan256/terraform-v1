2770ca9a05762c5a5d8a676c5365aa2f3d11ad64
Diego Mayorga <diego.mayorga86@gmail.com>
2020-04-15 19:49:50 -0300

    using variables and functions (workspaces)

---
+++ modulo7/outputs.tf
@@ -0,0 +1,10 @@
+# OUTPUTS
+###########################################################################
+ 
+ output "aws_elb_public_dns" {
+     value = aws_elb_web.dns_name
+ }
+
+ output "cname_record_url" {
+     value = "http://${local.env_name}-website.${var.dns_zone_name}"
+ }
