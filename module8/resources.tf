0715ec5d4b8f847a7e2c09ab2d50eaf12415931e
Diego Mayorga <diego.mayorga86@gmail.com>
2020-04-15 20:45:20 -0300

    creating module and testing modules from tf registry

--- module8/resources.tf
+++ module8/resources.tf
@@ -1,9 +1,9 @@
-# Providers
-###########################################################################
+# PROVIDERS
+##################################################################################
 
 provider "aws" {
-  #access_key = var.aws_access_key
-  #secret_key = var.aws_secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region     = var.region
 }
 
@@ -17,7 +17,7 @@ provider "azurerm" {
 }
 
 # DATA
-###########################################################################
+##################################################################################
 
 data "aws_availability_zones" "available" {}
 
@@ -41,63 +41,46 @@ data "aws_ami" "aws-linux" {
   }
 }
 
-# RESOURCES
-###########################################################################
+data "template_file" "public_cidrsubnet" {
+  count = var.subnet_count[terraform.workspace]
 
-# Random ID
+  template = "$${cidrsubnet(vpc_cidr,8,current_count)}"
+
+  vars = {
+    vpc_cidr      = var.network_address_space[terraform.workspace]
+    current_count = count.index
+  }
+}
+
+# RESOURCES
+##################################################################################
+
+#Random ID
 resource "random_integer" "rand" {
   min = 10000
   max = 99999
 }
 
 # NETWORKING #
-resource "aws_vpc" "vpc" {
-  cidr_block = var.network_address_space[terraform.workspace]
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name   = "${local.env_name}-vpc"
+  version = "2.15.0"
 
-  tags = merge(local.common_tags, { Name = "${local.env_name}-vpc" })
-}
+  cidr            = var.network_address_space[terraform.workspace]
+  azs             = slice(data.aws_availability_zones.available.names, 0, var.subnet_count[terraform.workspace])
+  public_subnets  = data.template_file.public_cidrsubnet[*].rendered
+  private_subnets = []
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-
-  tags = merge(local.common_tags, { Name = "${local.env_name}-igw" })
-  
-}
-
-resource "aws_subnet" "subnet" {
-  count                   = var.subnet_count[terraform.workspace]
-  cidr_block              = cidrsubnet(var.network_address_space[terraform.workspace], 8, count.index)
-  vpc_id                  = aws_vpc.vpc.id
-  map_public_ip_on_launch = true
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
-
-  tags = merge(local.common_tags, { Name = "${local.env_name}-subnet${count.index + 1}" })
-}
-
-# ROUTING #
-resource "aws_route_table" "rtb" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = merge(local.common_tags, { Name = "${local.env_name}-rtb" })
-}
-
-resource "aws_route_table_association" "rta-subnet" {
-  count          = var.subnet_count[terraform.workspace]
-  subnet_id      = aws_subnet.subnet[count.index].id
-  route_table_id = aws_route_table.rtb.id
+  tags = local.common_tags
 }
 
 # SECURITY GROUPS #
 resource "aws_security_group" "elb-sg" {
   name   = "nginx_elb_sg"
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = module.vpc.vpc_id
 
-  # Allow HTTP from anywhere
+  #Allow HTTP from anywhere
   ingress {
     from_port   = 80
     to_port     = 80
@@ -105,7 +88,7 @@ resource "aws_security_group" "elb-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow all outbound
+  #allow all outbound
   egress {
     from_port   = 0
     to_port     = 0
@@ -114,12 +97,13 @@ resource "aws_security_group" "elb-sg" {
   }
 
   tags = merge(local.common_tags, { Name = "${local.env_name}-elb-sg" })
+
 }
 
-# Nginx security group
+# Nginx security group 
 resource "aws_security_group" "nginx-sg" {
   name   = "nginx_sg"
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = module.vpc.vpc_id
 
   # SSH access from anywhere
   ingress {
@@ -146,13 +130,14 @@ resource "aws_security_group" "nginx-sg" {
   }
 
   tags = merge(local.common_tags, { Name = "${local.env_name}-nginx-sg" })
+
 }
 
 # LOAD BALANCER #
 resource "aws_elb" "web" {
   name = "${local.env_name}-nginx-elb"
 
-  subnets         = aws_subnet.subnet[*].id
+  subnets         = module.vpc.public_subnets
   security_groups = [aws_security_group.elb-sg.id]
   instances       = aws_instance.nginx[*].id
 
@@ -164,6 +149,7 @@ resource "aws_elb" "web" {
   }
 
   tags = merge(local.common_tags, { Name = "${local.env_name}-elb" })
+
 }
 
 # INSTANCES #
@@ -171,11 +157,11 @@ resource "aws_instance" "nginx" {
   count                  = var.instance_count[terraform.workspace]
   ami                    = data.aws_ami.aws-linux.id
   instance_type          = var.instance_size[terraform.workspace]
-  subnet_id              = aws_subnet.subnet[count.index % var.subnet_count[terraform.workspace]].id
+  subnet_id              = module.vpc.public_subnets[count.index % var.subnet_count[terraform.workspace]]
   vpc_security_group_ids = [aws_security_group.nginx-sg.id]
   key_name               = var.key_name
-  iam_instance_profile   = aws_iam_instance_profile.nginx_profile.name
-  depends_on             = [aws_iam_role_policy.allow_s3_all]
+  iam_instance_profile   = module.bucket.instance_profile.name
+  depends_on             = [module.bucket]
 
   connection {
     type        = "ssh"
@@ -208,7 +194,7 @@ EOF
     endscript
     lastaction
         INSTANCE_ID=`curl --silent http://169.254.169.254/latest/meta-data/instance-id`
-        sudo /usr/local/bin/s3cmd sync --config=/home/ec2-user/.s3cfg /var/log/nginx/ s3://${aws_s3_bucket.web_bucket.id}/nginx/$INSTANCE_ID/
+        sudo /usr/local/bin/s3cmd sync --config=/home/ec2-user/.s3cfg /var/log/nginx/ s3://${module.bucket.bucket.id}/nginx/$INSTANCE_ID/
     endscript
 }
 EOF
@@ -223,8 +209,8 @@ EOF
       "sudo cp /home/ec2-user/.s3cfg /root/.s3cfg",
       "sudo cp /home/ec2-user/nginx /etc/logrotate.d/nginx",
       "sudo pip install s3cmd",
-      "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/index.html .",
-      "s3cmd get s3://${aws_s3_bucket.web_bucket.id}/website/Globo_logo_Vert.png .",
+      "s3cmd get s3://${module.bucket.bucket.id}/website/index.html .",
+      "s3cmd get s3://${module.bucket.bucket.id}/website/Globo_logo_Vert.png .",
       "sudo cp /home/ec2-user/index.html /usr/share/nginx/html/index.html",
       "sudo cp /home/ec2-user/Globo_logo_Vert.png /usr/share/nginx/html/Globo_logo_Vert.png",
       "sudo logrotate -f /etc/logrotate.conf"
@@ -236,73 +222,22 @@ EOF
 }
 
 # S3 Bucket config#
-resource "aws_iam_role" "allow_nginx_s3" {
-  name = "${local.env_name}_allow_nginx_s3"
+module "bucket" {
+  name = local.s3_bucket_name
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_instance_profile" "nginx_profile" {
-  name = "${local.env_name}_nginx_profile"
-  role = aws_iam_role.allow_nginx_s3.name
-}
-
-resource "aws_iam_role_policy" "allow_s3_all" {
-  name = "${local.env_name}_allow_s3_all"
-  role = aws_iam_role.allow_nginx_s3.name
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:*"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-                "arn:aws:s3:::${local.s3_bucket_name}",
-                "arn:aws:s3:::${local.s3_bucket_name}/*"
-            ]
-    }
-  ]
-}
-EOF
-
-}
-
-resource "aws_s3_bucket" "web_bucket" {
-  bucket        = local.s3_bucket_name
-  acl           = "private"
-  force_destroy = true
-
-  tags = merge(local.common_tags, { Name = "${local.env_name}-web-bucket" })
-
+  source      = ".\\Modules\\s3"
+  common_tags = local.common_tags
 }
 
 resource "aws_s3_bucket_object" "website" {
-  bucket = aws_s3_bucket.web_bucket.bucket
+  bucket = module.bucket.bucket.id
   key    = "/website/index.html"
   source = "./index.html"
 
 }
 
 resource "aws_s3_bucket_object" "graphic" {
-  bucket = aws_s3_bucket.web_bucket.bucket
+  bucket = module.bucket.bucket.id
   key    = "/website/Globo_logo_Vert.png"
   source = "./Globo_logo_Vert.png"
 
