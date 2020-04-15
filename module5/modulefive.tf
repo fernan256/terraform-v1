2365c86f7155a3fab9c787e09ba8633b27a0680b
Diego Mayorga <diego.mayorga86@gmail.com>
2020-04-15 11:30:41 -0300

    modify s3 policy

--- module5/modulefive.tf
+++ module5/modulefive.tf
@@ -31,6 +31,7 @@ provider "aws" {
 }
 
 # LOCALS
+###########################################################################
 
 locals {
   common_tags = {
@@ -348,20 +349,20 @@ resource "aws_iam_role" "allow_nginx_s3" {
   name = "allow_nginx_s3"
 
   assume_role_policy = <<EOF
-	{
-		"Version": "2012-10-17",
-		"Statement": [
-			{
-				"Action": "sts:AssumeRole",
-				"Principal": {
-					"Service": "ec2.amazonaws.com"
-				},
-				"Effect": "Allow",
-				"Sid": ""
-			}
-		]
-	}
-	EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_iam_instance_profile" "nginx_profile" {
@@ -374,22 +375,23 @@ resource "aws_iam_role_policy" "allow_s3_all" {
   role = aws_iam_role.allow_nginx_s3.name
 
   policy = <<EOF
-	{
-		"Version": "2012-10-17",
-		"Statement": [
-			{
-				"Action": [
-					"s3:*"
-				],
-				"Effect": "Allow",
-				"Resource": [
-									"arn:aws:s3:::${local.s3_bucket_name}",
-									"arn:aws:s3:::${local.s3_bucket_name}/*"
-							]
-			}
-		]
-	}
-	EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+                "arn:aws:s3:::${local.s3_bucket_name}",
+                "arn:aws:s3:::${local.s3_bucket_name}/*"
+            ]
+    }
+  ]
+}
+EOF
+
 }
 
 resource "aws_s3_bucket" "web_bucket" {
